0aadaab1836e35324091c71274d596cfbaff8ca3
Julian <julian@juliantomlindesign.com>
2019-04-05 21:30:01 +0000

    added the stock market excercise

---
+++ stock-market.js
@@ -0,0 +1,37 @@
+var priceDirection=[]
+var cashOut = 0
+
+var trendMap = function (stockPrices){
+  for (var i = 0; i < stockPrices.length - 1; i++)
+    if (stockPrices[i] < stockPrices [i+1]) {
+      priceDirection.push(1)
+    }
+    else if (stockPrices[i] > stockPrices [i+1]) {
+      priceDirection.push(-1)
+    }
+  }
+
+var maxProfit = function(stockPrices){
+  trendMap(stockPrices)
+  for (var i = 0; i < stockPrices.length - 1; i++){
+    if ((priceDirection[i] === 1) && (i === (priceDirection.length-1))){
+      cashOut = cashOut + stockPrices[i+1]
+    }
+    if ((priceDirection[0] === 1) && (i === 0)){
+      cashOut = cashOut - stockPrices[i]
+    }
+    if ((priceDirection[i] === -1) && (priceDirection[i+1] === 1)){
+      cashOut = cashOut - stockPrices[i+1]
+    }
+    else if ((priceDirection[i] === 1) && (priceDirection[i+1] === -1)){
+      cashOut = cashOut + stockPrices[i+1]
+    }
+  }
+  if (cashOut == 0){
+    cashOut = -1
+  }
+  console.log(priceDirection)
+  return cashOut
+}
+
+module.exports = maxProfit
---
+++ stock_market-runner.js
@@ -0,0 +1,5 @@
+var maxProfit = require('./stock-market')
+
+input = [45, 24, 35, 31, 40, 38, 11, 20]
+
+console.log(maxProfit(input))
